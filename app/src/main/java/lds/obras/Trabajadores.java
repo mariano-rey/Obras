afe01633b8d2cf090c1b4d2195b501edd64c288d
Usuario <mariano.trey@gmail.com>
2017-07-14 14:26:10 -0300

    ASD

--- app/src/main/java/lds/obras/Trabajadores.java
+++ app/src/main/java/lds/obras/Trabajadores.java
@@ -1,13 +1,29 @@
 package lds.obras;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
 
 public class Trabajadores extends AppCompatActivity {
+
+    private RecyclerView rvTrabajadores;
+    private Button finalizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trabajadores);
+
+        finalizar = (Button) findViewById(R.id.finalizar);
+
+        rvTrabajadores = (RecyclerView) findViewById(R.id.rvTrabajadores);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        rvTrabajadores.setLayoutManager(linearLayoutManager);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, linearLayoutManager.getOrientation());
+        rvTrabajadores.addItemDecoration(dividerItemDecoration);
     }
 }
